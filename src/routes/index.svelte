7e55308c650342a691a588fe72dfadf4c14e6c45
josh <jloomb@gmail.com>
2022-07-20 00:12:07 +1200

    Add home page

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,107 +1,73 @@
 <div class="drawer drawer-end">
-  <input id="my-drawer-4" type="checkbox" class="drawer-toggle" />
-  <div class="drawer-content">
+  <input id="drawer" type="checkbox" class="drawer-toggle" />
+  <div class="drawer-content mx-auto">
     <!-- Page content here -->
 
 
-    <div class="preview border-base-300 bg-base-200 rounded-b-box rounded-tr-box flex min-h-[6rem] min-w-[18rem] max-w-4xl flex-wrap items-center justify-center gap-2 overflow-x-hidden border bg-cover bg-top p-4 undefined" style="background-size: 5px 5px">
-      <div>
-        <div class="grid w-32 h-20 rounded bg-primary text-primary-content place-content-center">1</div>
-        <div class="grid w-32 h-20 rounded bg-accent text-accent-content place-content-center">2</div>
+    <div class="flex flex-col w-11/12 mx-auto h-screen items-center overflow-x-hidden">
+
+      <div class="tag-wrap w-1/2 h-1/4 -mb-9 pr-8">
+        <div class="welcome-top h-full grid bg-accent content-end">
+          <img class="mx-auto pb-6" src="/ST_logo.png" alt="souptrippers logo"/>
+        </div> 
+      </div>
+
+      <div class="tag-wrap h-3/4 w-full pl-4">
+        <div class="welcome-bottom h-full pt-10 mx-3 flex flex-col flex-1 justify-between bg-accent text-accent-content content-end">
+          <div>
+            <h1>COMING SOON</h1>
+            <p></p>
+          </div>
+          <div>
+            <span class="tag-wrap m-6">
+              <label for="drawer" class="menu-button btn btn-secondary w-1/2">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="inline-block w-5 h-5 stroke-current">
+                  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16"></path>
+                </svg>
+              </label>
+            </span>
+          </div>
+
+        </div>
       </div>
     </div>
 
-    <div class="hero">
-      <figure>
-        <img src="https://unsplash.it/1600/900?image=830" alt="" />
-        <figcaption>
-          <h1>Angled Edge</h1>
-          <p>Image with CSS clip-path</p>
-        </figcaption>
-      </figure>
-    </div>
-    
-    <div class="promo">
-      <h1>Angled Edge</h1>
-      <p>CSS background with CSS clip-path</p>
-    </div>
-    <label for="my-drawer-4" class="drawer-button btn btn-primary">Open drawer</label>
   </div> 
   <div class="drawer-side">
     <label for="my-drawer-4" class="drawer-overlay"></label>
-    <ul class="menu p-4 overflow-y-auto w-80 bg-base-100 text-base-content">
+    <ul class="menu p-4 overflow-y-auto w-full bg-base-100 text-base-content">
       <!-- Sidebar content here -->
       <li><a>Sidebar Item 1</a></li>
       <li><a>Sidebar Item 2</a></li>
+      <label for="drawer" class="drawer-button btn btn-secondary">Close drawer</label>
     </ul>
   </div>
 </div>
 
 
+<style lang="scss">
+  // slanted edge here
+  .welcome-top {
+    -webkit-clip-path: polygon(0 0, 100% 0, 90% 90%, 10% 100%);
+    clip-path: polygon(0 0, 100% 0, 90% 90%, 10% 100%);
+    img {
+      width: 60%;
+    }
+  }
+  .welcome-bottom {
+    -webkit-clip-path: polygon(22% 5%, 80% 0%, 100% 100%, 0% 100%);
+    clip-path: polygon(22% 5%, 80% 0%, 100% 100%, 0% 100%);
+    overflow: hidden;
+    text-align: center;
+  }
 
-  <style lang="scss">
-    // slanted edge here
-    .hero img {
-      max-height: 200px;
-      -webkit-clip-path: polygon(0 0, 100% 0, 90% 90%, 10% 100%);
-      clip-path: polygon(0 0, 100% 0, 90% 90%, 10% 100%);
-    }
-    .promo {
-      -webkit-clip-path: polygon(22% 14%, 80% 0%, 100% 100%, 0% 100%);
-      clip-path: polygon(22% 14%, 80% 0%, 100% 100%, 0% 100%);
-    }
-  
-    // demo code below
-    .heroOFF {
-      color: #fff;
-      font-family: 'Fira Sans', sans-serif;
-      position: relative;
-      text-align: center;
-      text-shadow: 0px 0px 1px rgba(0,0,0,0.5);
-      
-      img {
-        width: 100%;
-      }
-      
-      figcaption {
-        left: 50%;
-        position: absolute;
-        top: 50%;
-        transform: translate(-50%, -50%);
-        width: 100%;
-      }
-      
-      h1 {
-        font-size: 32px;
-      }
-      
-      p {
-        font-size: 14px;
-        font-weight: 300;
-        margin-top: 0.5em;
-      }
-    }
-  
-    .promoOFF {
-      background: red;
-      background-size: cover;
-      color: #fff;
-      font-family: 'Fira Sans', sans-serif;
-      margin: 50px 0;
-      overflow: hidden;
-      padding: 150px 20px;
-      position: relative;
-      text-align: center;
-      text-shadow: 0px 0px 1px rgba(0,0,0,0.5);
-      
-      h1 {
-        font-size: 32px;
-      }
-      
-      p {
-        font-size: 14px;
-        font-weight: 300;
-        margin-top: 0.5em;
-      }
-    }
-  </style>
+.tag-wrap {
+  filter: drop-shadow(0 0 10px rgba(50, 50, 0, 0.5));
+} 
+
+.menu-button {
+  -webkit-clip-path: polygon(20% 0%, 80% 0%, 100% 20%, 100% 100%, 0 100%, 0% 20%);
+  clip-path: polygon(20% 0%, 80% 0%, 100% 20%, 100% 100%, 0 100%, 0% 20%);
+}
+
+</style>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -21,5 +21,5 @@ module.exports = {
   theme: {
     extend: {},
   },
-  plugins: [require("daisyui")],
+  plugins: [require("@tailwindcss/typography"), require("daisyui")],
 }
